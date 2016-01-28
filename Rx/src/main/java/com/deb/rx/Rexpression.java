09173ec76f90224f3212f72509043b36476fc99e
Debmalya Jash <debmalya.jash@gmail.com>
2016-01-29 00:53:10 +0800

    Added code and test code for setting attribute with its value in an element

--- Rx/src/main/java/com/deb/rx/Rexpression.java
+++ Rx/src/main/java/com/deb/rx/Rexpression.java
@@ -4,7 +4,10 @@
 package com.deb.rx;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,14 +17,45 @@ import java.util.regex.Pattern;
  */
 public class Rexpression {
 	
+	/**
+    *
+    * @param original String.
+    * @param expression expression to retrieve the desired element from the
+    * original string.
+    * @param attributeMap contains attribute name as key and corresponding
+    * value.
+    * @return original string with added attributes and their value.
+    */
+   public String addAttributeToElement(final String original, final String expression, final Map<String, String> attributeMap) {
+       String replacedString = original;
+       if (attributeMap != null && !attributeMap.isEmpty()) {
+           Set<Map.Entry<String, String>> attributeEntrySet = attributeMap.entrySet();
+           Iterator<Map.Entry<String, String>> attributeSetIterator = attributeEntrySet.iterator();
+           while (attributeSetIterator.hasNext()) {
+               Map.Entry<String, String> nextEntry = attributeSetIterator.next();
+               String value =  expression;
+               if (!value.startsWith("<")){
+            	   value = "<" + value;
+               }
+               replacedString = replacedString.replace(value, value + " " + nextEntry.getKey()+"=\""+nextEntry.getValue()+"\"");
+           }
+       }
+       return replacedString;
+   }
+
 	
 	
+	/**
+	 * 
+	 * @param original string
+	 * @param expression filter
+	 * @return list of string matching filter.
+	 */
 	public List<String> fliter(String original,String expression) {
 		List<String> matchingList = new ArrayList<>();
 		Pattern pattern = Pattern.compile(expression);
 		Matcher matcher = pattern.matcher(original);
 		while (matcher.find()) {
-//			System.out.println(matcher.group()+ " starts at " + matcher.start()+ " ends at " + matcher.end());
 			matchingList.add(matcher.group());
 		}
 		return matchingList;
--- Rx/src/test/java/com/deb/rx/RexpressionTest.java
+++ Rx/src/test/java/com/deb/rx/RexpressionTest.java
@@ -3,7 +3,9 @@
  */
 package com.deb.rx;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,17 +16,48 @@ import org.junit.Test;
  */
 public class RexpressionTest {
 
-	private static final String EXPECTED = "<love>Coding</love>";
 	/**
-	 * Test method for {@link com.deb.rx.Rexpression#fliter(java.lang.String, java.lang.String)}.
+	 * First expected string
+	 */
+	private static final String EXPECTED = "<love>Coding</love>";
+
+	private static final String original = "<life>" + EXPECTED + "</life>";
+
+	/**
+	 * Second expected string
+	 */
+	private static final String EXPECTED1 = "<life><love music=\"true\">Coding</love></life>";
+
+	/**
+	 * Test method for
+	 * {@link com.deb.rx.Rexpression#fliter(java.lang.String, java.lang.String)}
+	 * .
 	 */
 	@Test
 	public final void testFliter() {
 		Rexpression rpression = new Rexpression();
-		String original = "<life>" + EXPECTED + "</life>";
-		List<String> filteredList = rpression.fliter(original, "<love>.+</love>");
-		for (String each:filteredList) {
+
+		List<String> filteredList = rpression.fliter(original,
+				"<love>.+</love>");
+		for (String each : filteredList) {
 			Assert.assertEquals(EXPECTED, each);
+		}
+	}
+
+	@Test
+	public final void testAddAttributeToElement() {
+		Rexpression rpression = new Rexpression();
+
+		Map<String, String> attributeMap = new HashMap<String, String>();
+		attributeMap.put("music", "true");
+		for (int i = 0; i < 1000000; i++) {
+			String actual = rpression.addAttributeToElement(original, "love",
+					attributeMap);
+			Assert.assertEquals(EXPECTED1, actual);
+
+			actual = rpression.addAttributeToElement(original, "<love",
+					attributeMap);
+			Assert.assertEquals(EXPECTED1, actual);
 		}
 	}
 
