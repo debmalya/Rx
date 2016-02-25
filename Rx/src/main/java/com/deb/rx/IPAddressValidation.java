6a9e40a26599aa430127da19397375ed503858b8
Debmalya Jash <debmalya.jash@gmail.com>
2016-02-26 07:11:31 +0800

    Added new expression to validate IP address

---
+++ Rx/src/main/java/com/deb/rx/IPAddressValidation.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.deb.rx;
+
+/**
+ * @author debmalyajash
+ * 
+ * 
+ *         contain a string pattern.  Need to write a regular expression and
+ *         assign it to the pattern such that it can be used to validate an IP
+ *         address. 
+ */
+public class IPAddressValidation {
+	/**
+	 * IP Address validator expression.
+	 */
+	public static final String ipAddressValidatorExpression = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+}
